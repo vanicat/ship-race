762031fcc0959b95fd41d4529db82fca1b2e7230
Rémi Vanicat <vanicat@debian.org>
2020-09-19 07:08:56 +0200

    empty game

---
+++ main.js
@@ -0,0 +1,41 @@
+var game;
+
+
+var Starter = new Phaser.Class({
+    Extends: Phaser.Scene,
+
+    preload: function ()
+    {
+    },
+
+    create: function ()
+    {
+//        this.scene.add("game", MainGame, false);
+    },
+
+    update: function ()
+    {
+//        this.scene.start("menu", null);
+    }
+})
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 700,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            // debug: true,
+        }
+    },
+    scene: [ Starter ],
+    parent: "game",
+    textStyle: { fontSize: '32px', fill: '#000', boundsAlignH: "center", boundsAlignV: "middle" }
+};
+
+
+function main() {
+    game = new Phaser.Game(config);
+}
